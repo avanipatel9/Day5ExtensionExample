69c89e9e0f22d3a7568740d4dc713d52628fedb1
Avani Patel <anpatel36@yahoo.in>
2020-02-12 19:24:53 -0500

    formatting currency

--- Day5ExtensionExample/main.swift
+++ Day5ExtensionExample/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+    
+    func currency() -> String
+    {
+        let s = String.init(format: "$%.2f", self)
+        return s
+    }
+}
+
+var m = 100.0
+print(m.cm)
+print(m.currency())
+
+extension String
+{
+    func repeatNTimes(n:Int)
+    {
+        for _ in 1...n
+        {
+            print(self)
+        }
+    }
+}
+
+"Hello".repeatNTimes(n: 10)
 
