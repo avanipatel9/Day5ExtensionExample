c434500f7b4fc8a1b185d9677615bcde67a60d46
Avani Patel <anpatel36@yahoo.in>
2020-02-12 19:05:36 -0500

    Initial Commit

---
+++ Day5ExtensionExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day5ExtensionExample
+//
+//  Created by Avani Patel on 2/12/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
